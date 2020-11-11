79b0bc7e40607ce430a14b8a3bf6affe317e44b7
erdinctndgnn <47044071+erdinctndgnn@users.noreply.github.com>
2020-11-11 20:48:23 +0300

    Update usom_fraudalent_domain_search.py

--- usom_fraudalent_domain_search.py
+++ usom_fraudalent_domain_search.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re 
 list=[]
-for i in range(1,801) : #1 den 800 e kadar olan sayfalari tarar
+taranacak_sayfa=int(input("Taranacak Sayfa Sayısını Giriniz:"))
+for i in range(1,taranacak_sayfa+1) : #1'den istenen sayfaya kadar olan sayfaları tarar
     url="https://www.usom.gov.tr/zararli-baglantilar/"+str(i)+".html"
     html=requests.get(url).content
     soup=BeautifulSoup(html,"html.parser")
@@ -13,13 +14,24 @@ for i in range(1,801) : #1 den 800 e kadar olan sayfalari tarar
         sorgu=liste[i].text.splitlines()
         list.append(sorgu[2])
 
-aranacak_deger=input("Aramak istediginiz Veriyi Giriniz:")
-print(len(list)) #kac tane url tarandigini yazdirir
-
+aranacak_deger=input("Aramak İstediğiniz Veriyi Giriniz:")
+print(f"Taranan Sayfa Sayısı:{taranacak_sayfa}".format(taranacak_sayfa)) #kaç tane sayfa tarandığını yazdırır
+print(f"Taranan URL Sayısı:{len(list)}".format(len(list))) #kaç tane url tarandığını yazdırır
+sorgu_liste=[]
 def main() :
     for item in list :
-        if re.search(aranacak_deger,item) :
-            print(item,end="\t")
+        if re.search(aranacak_deger,item) : 
+            sorgu_liste.append(item) #bulunan sonuçlar liste içerisine eklenir
+        
 
 if __name__ == "__main__":
     main()
+
+if len(sorgu_liste) == 0 :
+    print("Sorgulanan Değer Bulunamadı...")
+else :
+    print(f"Bulunan Sonuç Sayısı:{len(sorgu_liste)}".format(len(sorgu_liste))) #bulunan sonuç sayısını yazdırır
+    print("Sonuçlar".center(50,"-"))
+    for sonuc in sorgu_liste :
+        print(sonuc) #bulunan sonuçları yazdırır
+
